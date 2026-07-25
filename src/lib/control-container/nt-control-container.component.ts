3f9a187fea76420c3b5e340d3da442e1de3cec07
Evgenii Grebennikov <djonnyx@gmail.com>
2026-07-26 02:17:43 +0300

    feat: unavailable properties for the control container are blocked

--- src/lib/control-container/nt-control-container.component.ts
+++ src/lib/control-container/nt-control-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, ViewEncapsulation } from "@angular/core";
+import { Component, inject, input, TemplateRef, ViewEncapsulation } from "@angular/core";
 import { IScrollViewService, NtVirtualScrollViewComponent, NtVirtualScrollViewService } from "../scroll-view";
 import { CONTROL_CONTAINER_SERVICE, SCROLL_VIEW_OVERSCROLL_ENABLED, SCROLL_VIEW_SERVICE, SCROLL_VIEW_USER_INTERACTION_ENABLED } from "../common";
 import { MOUSE_DOWN, MOUSE_MOVE, MOUSE_UP, TOUCH_END, TOUCH_MOVE, TOUCH_START, WHEEL } from "../common/const/event-names";
@@ -6,7 +6,6 @@ import { NtControlContainerService } from "./nt-control-container.service";
 import { takeUntilDestroyed, toObservable } from "@angular/core/rxjs-interop";
 import { filter, fromEvent, map, switchMap, tap } from "rxjs";
 import { IControlContainerService } from "./interfaces";
-import { validateBoolean } from "../common/utils/validation";
 
 /**
  * NtVirtualScrollViewComponent
@@ -33,22 +32,122 @@ import { validateBoolean } from "../common/utils/validation";
 export class NtControlContainerComponent extends NtVirtualScrollViewComponent<IScrollViewService> {
   protected _controlService = inject<IControlContainerService>(CONTROL_CONTAINER_SERVICE);
 
-  protected override _scrollbarEnabledOptions = {
-    transform: (v: boolean) => {
-      const valid = validateBoolean(v, true);
-
-      if (!valid) {
-        console.error('The "scrollbarEnabled" parameter must be of type `boolean`.');
-        return false;
-      }
-      return v;
+  protected override _scrollbarThickness = {
+    transform: (v: number) => {
+      console.error('The "scrollbarThickness" property is not available.');
+      return 0;
     },
   } as any;
 
   /**
-   * Determines whether the scrollbar is shown or not. The default value is "false".
+   * @deprecated
+   * The `scrollbarThickness` property is not available for the control container.
+   */
+  override scrollbarThickness = input<number>(0, { ...this._scrollbarThickness });
+
+  protected override _scrollbarMinSize = {
+    transform: (v: number) => {
+      console.error('The "scrollbarMinSize" property is not available.');
+      return 0;
+    },
+  } as any;
+
+  /**
+   * @deprecated
+   * The `scrollbarMinSize` property is not available for the control container.
+   */
+  override scrollbarMinSize = input<number>(0, { ...this._scrollbarMinSize });
+
+  protected override _scrollbarThumbRenderer = {
+    transform: (v: any) => {
+      console.error('The "scrollbarThumbRenderer" property is not available.');
+      return null;
+    },
+  } as any;
+
+  /**
+   * @deprecated
+   * The `scrollbarThumbRenderer` property is not available for the control container.
+   */
+  override scrollbarThumbRenderer = input<TemplateRef<any> | null>(null, { ...this._scrollbarThumbRenderer });
+
+  protected override _scrollbarThumbParams = {
+    transform: (v: { [propName: string]: any } | null) => {
+      console.error('The "scrollbarThumbParams" property is not available.');
+      return null;
+    },
+  } as any;
+
+  /**
+   * @deprecated
+   * The `scrollbarThumbParams` property is not available for the control container.
+   */
+  override scrollbarThumbParams = input<{ [propName: string]: any } | null>({}, { ...this._scrollbarThumbParams });
+
+  protected override _clickDistance = {
+    transform: (v: number) => {
+      console.error('The "clickDistance" property is not available.');
+      return 0;
+    },
+  } as any;
+
+  /**
+   * @deprecated
+   * The `clickDistance` property is not available for the control container.
+   */
+  override clickDistance = input<number>(0, { ...this._clickDistance });
+
+  protected override _scrollbarEnabledOptions = {
+    transform: (v: boolean) => {
+      console.error('The "scrollbarEnabled" property is not available.');
+      return false;
+    },
+  } as any;
+
+  /**
+   * @deprecated
+   * The `scrollbarEnabled` property is not available for the control container.
    */
   override scrollbarEnabled = input<boolean>(false, { ...this._scrollbarEnabledOptions });
+
+  protected override _scrollbarInteractiveOptions = {
+    transform: (v: boolean) => {
+      console.error('The "scrollbarInteractive" property is not available.');
+      return false;
+    },
+  } as any;
+
+  /**
+   * @deprecated
+   * The `scrollbarInteractive` property is not available for the control container.
+   */
+  override scrollbarInteractive = input<boolean>(false, { ...this._scrollbarInteractiveOptions });
+
+  protected override _overlappingScrollbarOptions = {
+    transform: (v: boolean) => {
+      console.error('The "overlappingScrollbar" property is not available.');
+      return false;
+    },
+  } as any;
+
+  /**
+   * @deprecated
+   * The `overlappingScrollbar` property is not available for the control container.
+   */
+  override overlappingScrollbar = input<boolean>(false, { ...this._overlappingScrollbarOptions });
+
+  protected override _overscrollEnabledOptions = {
+    transform: (v: boolean) => {
+      console.error('The "overscrollEnabled" property is not available.');
+      return false;
+    },
+  } as any;
+
+  /**
+   * @deprecated
+   * The `overscrollEnabled` property is not available for the control container.
+   */
+  override overscrollEnabled = input<boolean>(false, { ...this._overscrollEnabledOptions });
 
   constructor() {
     super();
