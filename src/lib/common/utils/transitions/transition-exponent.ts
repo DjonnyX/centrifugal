70ec9d176e81af8658c7bc494e450e63b8020309
Evgenii Grebennikov <djonnyx@gmail.com>
2026-08-02 06:09:28 +0300

    refactor: code review

--- src/lib/common/utils/transitions/transition-exponent.ts
+++ src/lib/common/utils/transitions/transition-exponent.ts
@@ -1,10 +1,12 @@
+import { DEFAULT_TRANSITION_EXPONENT } from "../../const/transitions";
+
 /**
  * transitionExponent
  * @link https://github.com/DjonnyX/centrifugal/blob/main/src/lib/common/utils/transitions/transition-exponent.ts
  * @author Evgenii Alexandrovich Grebennikov
  * @email djonnyx@gmail.com
  */
-export const transitionExponent = (value: number, max: number, exp: number = 1.5) => {
+export const transitionExponent = (value: number, max: number, exp: number = DEFAULT_TRANSITION_EXPONENT) => {
     const k = max !== 0 ? (value / max) : 0, kQuad = Math.pow(k, exp);
     return value * kQuad;
 }
