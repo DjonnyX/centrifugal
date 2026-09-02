1bcc2485f2fe34bb8878b6a4858e4cc95acf3d4d
Evgenii Grebennikov <djonnyx@gmail.com>
2026-09-02 15:02:04 +0300

    feat: added `centrifugal-file-viewer` example

---
+++ examples/file-viewer/src/app/shared/components/interfaces/gradient-color.ts
@@ -0,0 +1,9 @@
+import { Color } from "./color";
+
+/**
+ * GradientColor
+ * @link https://github.com/DjonnyX/centrifugal/blob/20.x/projects/centrifugal/src/lib/types/gradient-color.ts
+ * @author Evgenii Alexandrovich Grebennikov
+ * @email djonnyx@gmail.com
+ */
+export type GradientColor = [Color, Color];
---
+++ examples/file-viewer/src/app/shared/localization/localizations/interfaces/file-viewer-localization.ts
@@ -0,0 +1,10 @@
+import { IFileViewerHeaderLocalization } from "./file-viewer-header-localization";
+
+/**
+ * @author Evgenii Alexandrovich Grebennikov
+ * @email djonnyx@gmail.com
+ * @license Copyright (c) 2026 Evgenii Alexandrovich Grebennikov (djonnyx@gmail.com tg: http://t.me/djonnyx).
+ */
+export interface IFileViewerLocalization {
+    header: IFileViewerHeaderLocalization;
+}
