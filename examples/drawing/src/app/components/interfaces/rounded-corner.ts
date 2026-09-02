d17a403f1508bb5e7d4802f6c7bdbc0f077b0bbb
Evgenii Grebennikov <djonnyx@gmail.com>
2026-09-02 14:52:06 +0300

    feat: added `centrifugal-drawing` example

---
+++ examples/drawing/src/app/components/interfaces/rounded-corner.ts
@@ -0,0 +1,7 @@
+/**
+ * RoundedCorner
+ * @link https://github.com/DjonnyX/centrifugal/blob/main/projects/ng-virtual-list/src/lib/types/rounded-corner.ts
+ * @author Evgenii Alexandrovich Grebennikov
+ * @email djonnyx@gmail.com
+ */
+export type RoundedCorner = [number, number, number, number] | undefined;
---
+++ examples/drawing/src/app/widgets/painting-canvas/index.ts
@@ -0,0 +1,5 @@
+import { PaintingCanvasPageComponent } from './painting-canvas/painting-canvas.component';
+
+export {
+    PaintingCanvasPageComponent,
+}
