fcd269e5ea9f2cfcfac2338e257f98295d4114e6
Evgenii Grebennikov <djonnyx@gmail.com>
2026-09-02 15:23:13 +0300

    feat: added `centrifugal-gallery` example

---
+++ examples/gallery/src/app/shared/models/pages/gallery-image.model.ts
@@ -0,0 +1,11 @@
+import { Id } from 'centrifugal';
+
+/**
+ * @author Evgenii Alexandrovich Grebennikov
+ * @email djonnyx@gmail.com
+ */
+export interface IGalleryImageData {
+  id: Id;
+  text: string;
+  img?: string;
+}
